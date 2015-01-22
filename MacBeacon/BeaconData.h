e27542f962573f3d6863c72cf6b0320a9e6ef6b7
AyakaTominaga <alto.tone@gmail.com>
2015-01-22 11:11:49 +0900

    Review of the license
    This software is released under the MIT License, see LICENSE.txt.

--- MacBeacon/BeaconData.h
+++ MacBeacon/BeaconData.h
@@ -3,7 +3,10 @@
 //  MacBeacon
 //
 //  Created by Ayaka Tominaga on 2014/11/24.
-//  Copyright (c) 2014年 Communication Planning Corporation. All rights reserved.
+//  Copyright (c) 2014年 Ayaka Tominaga
+//
+//  This software is released under the MIT License.
+//  http://opensource.org/licenses/mit-license.php
 //
 
 #import <Foundation/Foundation.h>
